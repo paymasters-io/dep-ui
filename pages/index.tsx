48c2fae02db68b0787692db5a5ff712e24dda7c7
miracleonyenma <miracleficient@gmail.com>
2023-04-04 11:10:32 +0100

    WIP update: payamsters dep-ui index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ import styles from "@/pages/index.module.css";
 import DefaultLayout from "layouts/Default";
 import { ReactElement } from "react";
 
+import zetachainImg from "../assets/icons/zetachain.png";
+import { CheckIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
 export const getStaticProps = () => {
   return {
     props: {
@@ -16,7 +19,7 @@ export const getStaticProps = () => {
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Paymaster</title>
         <meta
@@ -51,9 +54,52 @@ export default function Home() {
         <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <main className="app-main">
-        <h1>Coming Soon</h1>
+        <section className="app-section">
+          <div className="wrapper">
+            <header className="app-section-header">
+              <h2>Active Paymaster</h2>
+            </header>
+            <article className="paymaster-card">
+              <div className="wrapper">
+                <header className="paymaster-card-header">
+                  <figure className="img-cont">
+                    <Image
+                      src={zetachainImg}
+                      width={36}
+                      height={36}
+                      alt="zeta"
+                    />
+                  </figure>
+                  <div className="paymaster-info">
+                    <h3 className="font-bold">Zetachain</h3>
+                    <p>
+                      The only public an decentralized blockchain that enables
+                      message passing and...
+                    </p>
+                  </div>
+                </header>
+                <div className="status">
+                  <div className="cta no-bg !gap-1">
+                    <span className="text text-white">Active</span>
+                    <CheckIcon className="icon !w-4 !h-4" />
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+        </section>
+        <div className="app-section">
+          <div className="wrapper pb-16 border-b border-[#FFFFFF12]">
+            <div className="form-control">
+              <div className="input-cont form-input grad-border" data-type="text">
+                <MagnifyingGlassIcon className="icon" />
+                <input type="text" placeholder="Search paymasters"/>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
-    </div>
+    </>
   );
 }
 
