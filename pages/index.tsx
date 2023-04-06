a1021fc0a41c1a64a38179759e3203f5d2ce958d
miracleonyenma <miracleficient@gmail.com>
2023-04-06 15:10:36 +0100

    minor: fix switch and button colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -191,7 +191,7 @@ export default function Home() {
                         </ul>
                       </details>
                       <div className="action-cont">
-                        <button className="cta">Use</button>
+                        <button className="cta !bg-paymasters-purple/20">Use</button>
                       </div>
                     </div>
                   </article>
